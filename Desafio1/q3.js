ba36ad69cadeda3224d486c96650407c916708c2
hellemmendoncaa <hellemmendonca40@gmail.com>
2025-02-11 08:00:12 -0300

    q3.js

--- Desafio1/q3.js
+++ Desafio1/q3.js
@@ -4,5 +4,5 @@ let preco = 50;
 let desconto = 0.2;
 let precoComDesconto;
 
-precoComDesconto = preco - desconto;
+precoComDesconto = preco * (1 - desconto);
 console.log('Com o desconto de 20%, sua compra ficou no valor de ' + precoComDesconto + 'R$.');
